d2ac6a5469d0d56770beb8e255907b2a97457847
Aris Bimas Nugroho <arisbimas2@gmail.com>
2023-11-01 22:19:11 +0700

    feat: loading page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,15 @@
-import React from 'react'
+"use client"
+import { FileSearch } from '@phosphor-icons/react'
+import Link from 'next/link'
 
 export default function Page() {
     return (
-        <div className='text-brand-accent'>Not Found</div>
+        <div className='min-h-screen max-w-xl mx-auto flex justify-center items-center'>
+            <div className='flex justify-center items-center gap-4 flex-col'>
+                <FileSearch size={32} className='text-brand-accent ' />
+                <h3 className='text-brand-accent text-4xl font-bold'>Not Found</h3>
+                <Link href={"/"} className='text-brand-primary hover:text-brand-accent underline'>Kembali</Link>
+            </div>
+        </div>
     )
 }
